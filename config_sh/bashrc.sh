87fb9604d5b986a037606f91e6ffd08bf32b366d
Corentin Giroud-Argoud <giroud_c@epita.fr>
2018-08-20 05:04:07 +0200

    Use own bashrc and vimrc repos

--- config_sh/bashrc.sh
+++ config_sh/bashrc.sh
@@ -3,19 +3,10 @@
 LOCAL_REPO="${HOME}/.bash"
 
 if [ ! -d "$LOCAL_REPO" ]; then
-  git clone --depth=1 https://github.com/jaivardhankapoor/bestbash "$LOCAL_REPO"
+  git clone git@github.com:ninichat/bashrc.git "$LOCAL_REPO"
 else
   cd "$LOCAL_REPO"
-  git stash
-  git pull --depth=1
-  git stash pop
+  git pull
   cd -
 fi
 ln -sf "${LOCAL_REPO}/init" "${HOME}/.bashrc"
-
-SRC_PATH=$(dirname "$0")
-
-# Custom aliases
-cp "${SRC_PATH}/custom_alias" "${LOCAL_REPO}/custom_alias"
-
-echo "source ${LOCAL_REPO}/custom_alias" >> "${HOME}/.bashrc"
--- config_sh/vimrc.sh
+++ config_sh/vimrc.sh
@@ -3,12 +3,10 @@
 LOCAL_REPO="${HOME}/.vim_runtime"
 
 if [ ! -d "$LOCAL_REPO" ]; then
-  git clone --depth=1 git@github.com:ninichat/vimrc.git "$LOCAL_REPO"
+  git clone git@github.com:ninichat/vimrc.git "$LOCAL_REPO"
 else
   cd "$LOCAL_REPO"
-  git stash
-  git pull --depth=1
-  git stash pop
+  git pull
   cd -
 fi
 sh "${LOCAL_REPO}/install_awesome_vimrc.sh"
